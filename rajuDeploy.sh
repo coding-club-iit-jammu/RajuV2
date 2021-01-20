b75a1055abed9cdcf700ea7dce4abf5ff96dff55
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2021-01-20 21:50:31 +0530

    deploy.sh loc changed

--- rajuDeploy.sh
+++ rajuDeploy.sh
@@ -1,6 +1,6 @@
 pwd
 sudo pm2 delete raju
-cp -r /home/azureuser/azagent/_work/r1/a/_coding-club-iit-jammu_RajuV2/* /home/azureuser/RajuV2/
+cp -r /home/azureuser/azagent/_work/r2/a/_coding-club-iit-jammu_RajuV2/* /home/azureuser/RajuV2/
 cd /home/azureuser/RajuV2/
 pip3 install -r reqirements.txt
 sudo pm2 -f start raju.py --no-autorestart --interpreter python3
