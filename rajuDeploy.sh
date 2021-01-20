6f03dbbaaec2beaca22758ccdaf675f409abbbad
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2021-01-20 21:52:23 +0530

    typo

--- rajuDeploy.sh
+++ rajuDeploy.sh
@@ -2,7 +2,7 @@ pwd
 sudo pm2 delete raju
 cp -r /home/azureuser/azagent/_work/r2/a/_coding-club-iit-jammu_RajuV2/* /home/azureuser/RajuV2/
 cd /home/azureuser/RajuV2/
-pip3 install -r reqirements.txt
+pip3 install -r requirements.txt
 sudo pm2 -f start raju.py --no-autorestart --interpreter python3
 sudo pm2 startup systemd
 sudo pm2 save
