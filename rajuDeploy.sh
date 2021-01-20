8f20735b9bcc289320682c5a731045c5bf8e7952
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2021-01-20 22:04:59 +0530

    venv

--- rajuDeploy.sh
+++ rajuDeploy.sh
@@ -2,7 +2,9 @@ pwd
 sudo pm2 delete raju
 cp -r /home/azureuser/azagent/_work/r2/a/_coding-club-iit-jammu_RajuV2/* /home/azureuser/RajuV2/
 cd /home/azureuser/RajuV2/
-pip3 install -r requirements.txt
-sudo pm2 -f start raju.py --no-autorestart --interpreter python3
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+sudo pm2 -f start raju.py --no-autorestart --interpreter /home/azureuser/RajuV2/venv/bin/python
 sudo pm2 startup systemd
 sudo pm2 save
